237eebc270ea653a777f1a58719f7c60f85ce5ee
Chan Staples <chanstaples1@gmail.com>
2021-06-20 12:02:13 -0400

    Update stock_return.py

--- stock_return.py
+++ stock_return.py
@@ -1,3 +1,11 @@
+#stock_return.py
+#Created by: Chan Staples
+#Date: 06/18/2021
+
+##############################################################################################################################################################################################
+
+#Import Statements
+
 import sys
 import requests
 import json
@@ -6,6 +14,11 @@ from datetime import date
 from help import *
 import webbrowser
 
+##############################################################################################################################################################################################
+
+#Script Execution Section
+
+#This section retrieves user information.
 ticker = get_ticker()
 year = get_user_year(ticker)
 month = get_user_month(ticker,year)
@@ -18,11 +31,13 @@ second_user_datetime = getStartDateTime2(date1)
 current_day = datetime.now().timestamp()
 current_day = int(current_day)
 
+#This section prepares program to retrieve information from Yahoo Finance.
 URL="https://finance.yahoo.com/quote/{}/history?period1={}&period2={}&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true".format(ticker,user_datetime,current_day)
 #webbrowser.open(URL)
 currentDayEndPrice = 0
 userEndPrice = 0
 
+#This section retrieves information from Yahoo Finance.
 try:
 	data = requests.get(URL).text
 	firstSplit = data.split(str(user_datetime))[9]
@@ -42,12 +57,19 @@ try:
 except:
 	print("{} has an issue with ticker {}.".format(date1,ticker))
 
+#This section prints the results.
 try:
 	userSharePrice = userEndPrice * shares
 	currentSharePrice = currentDayEndPrice * shares
-	profit = getProfit(userSharePrice,currentSharePrice)
+	profit = currentSharePrice - userSharePrice
+	profit = fixProfit(profit)
 	print("Total Gross Profit for {} for {} shares bought on {} is ${}.\n".format(ticker,shares,date1,profit))
+	getProfitAfterTax(year,month,day,profit,ticker,shares)
 except:
 	print("ERROR! Profits not calculated correctly for ticker {}.".format(ticker))
 
+##############################################################################################################################################################################################
 
+#EOF
+
+##############################################################################################################################################################################################
